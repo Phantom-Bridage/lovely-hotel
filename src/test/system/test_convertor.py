345d8e70a00dbb0e145c30bf61fc2c901ed7ce1e
knskw <6westry@gmail.com>
2014-11-15 15:23:21 +0900

    データベースマネージャー(HotelSystemDataSet)作成 ダミーのデータ取得後、パケットにして返すところまで

---
+++ src/test/system/test_convertor.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import unittest2
+
+
+class TestConverter(unittest2.TestCase):
+    def test_that_should_list(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest2.main()
---
+++ src/test/system/test_hotel_system_data_set.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import unittest2
+
+
+class WhenCreatingHotelSystem(unittest2.TestCase):
+    def _get_target_class(self):
+        from system.hotel_system_data_set import HotelSystemDataSet
+
+        return HotelSystemDataSet
+
+    def _make_one(self):
+        return self._get_target_class()()
+
+    def test_that_should_packet(self):
+        dm = self._make_one()
+        self.assertMultiLineEqual(
+            '1,knskw,knskw@nullnull.com,檸檬根井都武者返市愛宿町4-2-3 エレガンスウェストハイツ999号室,ビジネス,9999,1,ダンボール,みかん,14/11/14/Fri,15,14/11/15/Sat',
+            dm.packet(1))
+
+
+if __name__ == "__main__":
+    unittest2.main()
