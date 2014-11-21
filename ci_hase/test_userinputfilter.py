007f83b3632e59cb416ec3329079a59195a1db5f
tsukuyomip <ab0312uo@gmail.com>
2014-11-21 17:07:37 +0900

    テストメソッドの分離とプロダクトコードのリファクタリング（軽いやつ）
    * テストメソッドをより細分化することで
      何処でテストが通らなかったかわかりやすい
    * 重複部分を削った．
        * モジュール化できるか？

--- ci_hase/test_userinputfilter.py
+++ ci_hase/test_userinputfilter.py
@@ -8,64 +8,98 @@ class TestUserInputFilter(unittest.TestCase):
     def test_decide_correct_addres_format(self):
         result = uif.check_address("東京都新宿区1-1-1")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
 
     def test_detect_wrong_address_format(self):
         result = uif.check_address("1-1-1")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 0)
 
 
-    def test_decide_correct_email_format(self):
+    def test_decide_correct_email_format_dot_com(self):
         result = uif.check_email("unagi@nullnull.com")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
+
+    def test_decide_correct_email_format_dot_jp(self):
         result = uif.check_email("unagi@nullnull.co.jp")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(),-1)
+
+    def test_decide_correct_email_format_dot_foo_dot_co_dot_jp(self):
         result = uif.check_email("unagi@student.miyazaki-u.ac.jp")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
 
     def test_detect_include_nbsp_in_email(self):
         result = uif.check_email("u na gi@nullnull.com")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 4)
 
     def test_detect_include_2bite_charactor_on_head_of_email(self):
         result = uif.check_email("うなgi@nullnull.com")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 5)  # unicodeは3バイトコード
 
     def test_detect_include_2bite_charactor_on_middle_of_email(self):
         result = uif.check_email("unagi@nullヌル.com")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 10)
 
     def test_detect_include_2bite_charactor_on_tail_of_email(self):
         result = uif.check_email("unagi@nullnull.coむ")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 17)
 
-    def test_detect_include_taboo_string_in_email(self):
+    def test_detect_include_taboo_string_in_email_dot_at(self):
         result = uif.check_email("unagi.@nullnull.com")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 5)
+
+    def test_detect_include_taboo_string_in_email_dot_dot(self):
         result = uif.check_email("unagi@nullnull..com")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 14)
 
-
-    def test_decide_correct_name_format(self):
+    def test_decide_correct_name_nbsp_name_format_english(self):
         result = uif.check_name("Rin Hoshizora")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
+
+    def test_decide_correct_name_nbsp_name_format_japanese(self):
         result = uif.check_name("星空 凛")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
+
+    def test_decide_correct_namename_format_english(self):
         result = uif.check_name("RinHoshizora")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
+
+    def test_decide_correct_namename_format_japanese(self):
         result = uif.check_name("星空凛")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
+
+    def test_decide_correct_name_nbsp_name_nbsp_name_format_english(self):
         result = uif.check_name("John F. Kennedy")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
+
+    def test_decide_correct_name_nth_format_english(self):
         result = uif.check_name("Lupin III")
         self.assertTrue(result.get_result())
+        self.assertEqual(result.get_index(), -1)
 
-    def test_detect_include_nubmer_in_name(self):
+    def test_detect_include_number_in_name(self):
         result = uif.check_name("Lupin 3rd")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 6)
 
     def test_detect_too_long_name(self):
         result = uif.check_name("Captain Fantastic Faster Than Superman Spiderman Batman Wolverine Hulk And The Flash Combined")
         self.assertFalse(result.get_result())
+        self.assertEqual(result.get_index(), 30)
 
     #def test_detect_wrong_character_in name(self):
     #    result = uif.check_name("☆")
--- ci_hase/util/userinputfilter.py
+++ ci_hase/util/userinputfilter.py
@@ -9,38 +9,33 @@ import util.formatcheckresult as fcr
 
 
 def check_address(address):
-    m = re.search(
-        "[亜-熙ぁ-んァ-ヶA-Za-z]+", 
-        address)
+    (result, index, comment) = (True, -1, "")
+    m = re.search("[亜-熙ぁ-んァ-ヶA-Za-z]+", address)
     if m is None:
-        result = fcr.FormatCheckResult(result = False, index = 0, comment = "Wrong address format")
-    else:
-        result = fcr.FormatCheckResult(result = True, index = 0, comment = "")
-    return result
+        (result, index, comment) = (False, 0, "Wrong address format")
+    return fcr.FormatCheckResult(result, index, comment)
 
 def check_email(email):
+    (result, index, comment) = (True, -1, "")
     m = re.search("[A-Za-z0-9._-]+@[A-Za-z]+(.[A-Za-z]+)+", email)
     if m.start() != 0:
-        result = fcr.FormatCheckResult(result = False, index = 0, comment = "Wrong email format")
+        (result, index, comment) = (False, m.start() - 1, "Wrong email format")
     elif m.end() != len(email):
-        result = fcr.FormatCheckResult(result = False, index = m.end(), comment = "Wrong email format")
+        (result, index, comment) = (False, m.end(), "Wrong email format")
     else:
         m = re.search("\.\.|\.@", email)
-        if m is None:
-            result = fcr.FormatCheckResult(result = True, index = 0, comment = "")
-        else:
-            result = fcr.FormatCheckResult(result = False, index = m.start(), comment = "Wrong email format. Cannnot use '..' or '.@'.")
-    return result
+        if m is not None:
+            (result, index, comment) = (False, m.start(), "Wrong email format. Cannnot use '..' or '.@'.")
+    return fcr.FormatCheckResult(result, index, comment)
 
 def check_name(name):
+    (result, index, comment) = (True, -1, "")
     if len(name) >= 30:
-        return fcr.FormatCheckResult(result = False, index = 30, comment = "Too long name")
-
+        return fcr.FormatCheckResult(False, 30, "Too long name")
     m = re.search("[A-Za-z亜-熙ぁ-んァ-ヶ .]+", name)
     if m.start() != 0:
-        result = fcr.FormatCheckResult(result = False, index = 0, comment = "Wrong name")
+        (result, index, comment) = (False, m.start() - 1, "Wrong name")
     elif m.end() != len(name):
-        result = fcr.FormatCheckResult(result = False, index = m.end(), comment = "Wrong name")
-    else:
-        result = fcr.FormatCheckResult(result = True, index = 0, comment = "")
-    return result
+        (result, index, comment) = (False, m.end(), "Wrong name")
+
+    return fcr.FormatCheckResult(result, index, comment)
