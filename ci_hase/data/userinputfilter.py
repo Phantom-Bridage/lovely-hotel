e98a5a0a32eeff8e9d1911a44a2683b08c74e282
tsukuyomip <ab0312uo@gmail.com>
2014-11-21 11:27:17 +0900

    書式チェック用パッケージ userinputfilter を追加

---
+++ ci_hase/data/userinputfilter.py
@@ -0,0 +1,44 @@
+#coding: utf-8
+
+import re
+import data.formatcheckresult as fcr
+
+from dataexception import DataException
+
+# re.match("[亜-熙ぁ-んァ-ヶ]+", "日本語の範囲")
+# re.match("[A-Za-z]+", "English")
+# re.match("[0-9]+", "0123456789")
+
+
+def check_address(address):
+    m = re.search(
+        "[亜-熙ぁ-んァ-ヶA-Za-z]+", 
+        address)
+    if m is None:
+        result = fcr.FormatCheckResult(result = False, index = 0, comment = "Wrong address format")
+    else:
+        result = fcr.FormatCheckResult(result = True, index = 0, comment = "")
+    return result
+
+def check_email(email):
+    m = re.search("[A-Za-z0-9._-]+@[A-Za-z]+(.[A-Za-z]+)+", email)
+    if m.start() != 0:
+        result = fcr.FormatCheckResult(result = False, index = 0, comment = "Wrong email format")
+    elif m.end() != len(email):
+        result = fcr.FormatCheckResult(result = False, index = m.end(), comment = "Wrong email format")
+    else:
+        result = fcr.FormatCheckResult(result = True, index = 0, comment = "")
+    return result
+
+def check_name(name):
+    if len(name) >= 30:
+        return fcr.FormatCheckResult(result = False, index = 30, comment = "Too long name")
+
+    m = re.search("[A-Za-z亜-熙ぁ-んァ-ヶ .]+", name)
+    if m.start() != 0:
+        result = fcr.FormatCheckResult(result = False, index = 0, comment = "Wrong name")
+    elif m.end() != len(name):
+        result = fcr.FormatCheckResult(result = False, index = m.end(), comment = "Wrong name")
+    else:
+        result = fcr.FormatCheckResult(result = True, index = 0, comment = "")
+    return result
---
+++ ci_hase/test_userinputfilter.py
@@ -0,0 +1,71 @@
+#coding: utf-8
+
+import unittest
+import data.userinputfilter as uif
+
+
+class TestUserInputFilter(unittest.TestCase):
+    def test_decide_correct_addres_format(self):
+        result = uif.check_address("東京都新宿区1-1-1")
+        self.assertTrue(result.get_result())
+
+    def test_detect_wrong_address_format(self):
+        result = uif.check_address("1-1-1")
+        self.assertFalse(result.get_result())
+
+
+    def test_decide_correct_email_format(self):
+        result = uif.check_email("unagi@nullnull.com")
+        self.assertTrue(result.get_result())
+        result = uif.check_email("unagi@nullnull.co.jp")
+        self.assertTrue(result.get_result())
+        result = uif.check_email("unagi@student.miyazaki-u.ac.jp")
+        self.assertTrue(result.get_result())
+
+    def test_detect_include_nbsp_in_email(self):
+        result = uif.check_email("u na gi@nullnull.com")
+        self.assertFalse(result.get_result())
+
+    def test_detect_include_2bite_charactor_on_head_of_email(self):
+        result = uif.check_email("うなgi@nullnull.com")
+        self.assertFalse(result.get_result())
+
+    def test_detect_include_2bite_charactor_on_middle_of_email(self):
+        result = uif.check_email("unagi@nullヌル.com")
+        self.assertFalse(result.get_result())
+
+    def test_detect_include_2bite_charactor_on_tail_of_email(self):
+        result = uif.check_email("unagi@nullnull.coむ")
+        self.assertFalse(result.get_result())
+
+
+    def test_decide_correct_name_format(self):
+        result = uif.check_name("Rin Hoshizora")
+        self.assertTrue(result.get_result())
+        result = uif.check_name("星空 凛")
+        self.assertTrue(result.get_result())
+        result = uif.check_name("RinHoshizora")
+        self.assertTrue(result.get_result())
+        result = uif.check_name("星空凛")
+        self.assertTrue(result.get_result())
+        result = uif.check_name("John F. Kennedy")
+        self.assertTrue(result.get_result())
+        result = uif.check_name("Lupin III")
+        self.assertTrue(result.get_result())
+
+    def test_detect_include_nubmer_in_name(self):
+        result = uif.check_name("Lupin 3rd")
+        self.assertFalse(result.get_result())
+
+    def test_detect_too_long_name(self):
+        result = uif.check_name("Captain Fantastic Faster Than Superman Spiderman Batman Wolverine Hulk And The Flash Combined")
+        self.assertFalse(result.get_result())
+
+    #def test_detect_wrong_character_in name(self):
+    #    result = uif.check_name("☆")
+    #    self.assertFalse(result.get_result())
+
+
+
+if __name__ == "__main__":
+    unittest.main()
