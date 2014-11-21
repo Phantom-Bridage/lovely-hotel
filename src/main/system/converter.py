7eb519a30e9b7cf8996f864f0f299aca469935d6
knskw <6westry@gmail.com>
2014-11-21 12:17:25 +0900

    壇蜜を窓口に変えて、少々手直し(テスト書いてない。。。)

--- src/main/system/converter.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-class Converter:
-    @classmethod
-    def to_str(self, sql_data):
-        lst = []
-        for i, it in enumerate(map(str, sql_data)):
-            lst.append(it)
-        return lst
