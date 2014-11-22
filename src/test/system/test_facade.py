2b01c72fe82fefe0e10f254f03ded097b9f8a8b2
knskw <6westry@gmail.com>
2014-11-23 03:01:00 +0900

    python版hamcrestでテストが動作するかの確認

--- src/test/system/test_facade.py
+++ src/test/system/test_facade.py
@@ -1,8 +1,12 @@
 # coding: utf-8
-import unittest2
+import unittest
+from hamcrest import *
 
+class WhenCreatingHotelSystem(unittest.TestCase):
+    def setUp(self):
+        self.expected = '1,knskw,knskw@nullnull.com,檸檬根井都武者返市愛宿町4-2-3 エレガンスウェストハイツ999号室,ビジネス,9999,1,ダンボール,みかん,14/11/14/Fri,15,14/11/15/Sat'
+        self.actual = self._get_target_instance().packet(1)
 
-class WhenCreatingHotelSystem(unittest2.TestCase):
     def _get_target_class(self):
         from system.facade import Facade
 
@@ -12,10 +16,9 @@ class WhenCreatingHotelSystem(unittest2.TestCase):
         return self._get_target_class()()
 
     def test_that_should_packet(self):
-        dm = self._get_target_instance()
-        self.assertMultiLineEqual(
-            '1,knskw,knskw@nullnull.com,檸檬根井都武者返市愛宿町4-2-3 エレガンスウェストハイツ999号室,ビジネス,9999,1,ダンボール,みかん,14/11/14/Fri,15,14/11/15/Sat',
-            dm.packet(1))
+        assert_that(self.actual,is_(self.expected))
+        #self.assertMultiLineEqual(self.actual, self.expected)
+
 
 if __name__ == "__main__":
-    unittest2.main()
+    unittest.main()
